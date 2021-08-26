66690222c62c2f402def4765b4cfe538b47731de
Helen <helen.harmer.2000@gmail.com>
2021-08-26 12:06:14 +0100

    CRUD features for pack working

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect
 from flaskr.models import Pack
 from flaskr.models import Cards
 
+from flaskr.database import init_db
 
 
 def create_app(test_config=None):
@@ -51,19 +52,57 @@ def create_app(test_config=None):
         
         if request.method =='POST':
             print("HERE")
+            
             pack_name = request.form['pname']
             new_pack = Pack(name=pack_name)
             print(pack_name)
             print("NEW PACK", new_pack)
             try:
-                db.session.add(new_pack)
-                db.session.commit()
+                init_db()
+                from flaskr.database import db_session
+                db_session.add(new_pack)
+                db_session.commit()
                 return redirect('/')
             except Exception as e:
                 print(e)
                 return ' there was an issue adding this to your database'
         else:
             return render_template('createPack.html')
+
+    @app.route('/deletePack/<int:id>')
+    def deletePack(id):
+        pack_to_delete = Pack.query.filter(Pack.id == id).first()
+        try:
+            cards_to_delete = Pack.query.filter(Cards.pack_id==id).all()
+            init_db()
+            from flaskr.database import db_session
+            db_session.delete(pack_to_delete)
+            for each in cards_to_delete:
+                db_session.delete(each)
+            db_session.commit()
+            return redirect('/')
+        except:
+            return "there is a problem deleting this pack"
+    
+    @app.route('/editPack/<int:id>', methods=['POST', 'GET'])
+    def editPack(id):
+        pack_to_edit = Pack.query.filter(Pack.id == id).first()
+        if request.method =='POST':
+            print("HERE")
+            pack_to_edit.name = request.form['pname']
+            
+            try:
+                init_db()
+                from flaskr.database import db_session
+                db_session.commit()
+                return redirect('/')
+            except Exception as e:
+                print(e)
+                return ' there was an issue editing your database'
+        else:
+            return render_template('editPack.html', pack = pack_to_edit)
+
+
 
     return app
 
